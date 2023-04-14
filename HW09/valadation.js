270362146c8b02cc44fb7298e4b58b8feb53ec4f
URCOzxc <urco.dm@gmail.com>
2023-04-13 22:05:00 -0400

    WORK

--- HW09/valadation.js
+++ HW09/valadation.js
@@ -1,18 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
+function setvar() {
     var cutbutton = document.getElementById("cutbox");
+    cutbutton.addEventListener('click', disablebutton(cutbutton));
+}
   
-    function disablebutton() {
-      var darkElements = document.getElementsByClassName("dark");
-      for (var i = 0; i < darkElements.length; i++) {
-          if(cutbutton.checked) {
-              darkElements[i].disabled = false;
-          }
-          if(!cutbutton.checked) {
-              darkElements[i].disabled = true;
-              darkElements[i].checked = false;
-          }
-      }
+function disablebutton(cutbutton){
+    var darkElements = document.getElementsByClassName("dark");
+
+    for (var i = 0; i < darkElements.length; i++) {
+        if(cutbutton.checked) {
+            darkElements[i].disabled = false;
+        }
+        if(!cutbutton.checked) {
+            darkElements[i].disabled = true;
+            darkElements[i].checked = false;
+        }
     }
-  
-    cutbutton.addEventListener('click', disablebutton);
-  });
+}
+
+function formmagic() {
+    setvar();
+    disablebutton();
+}
+
+document.addEventListener("DOMContentLoaded", formmagic)
