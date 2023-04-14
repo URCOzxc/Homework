855c13b1e2f21e239399aa48eaa3bdd7a40ee0dd
URCOzxc <urco.dm@gmail.com>
2023-04-13 20:59:24 -0400

    finished 3

--- HW09/valadation.js
+++ HW09/valadation.js
@@ -1,3 +1,8 @@
 function disablebutton() {
-    
+    if(document.getElementById("cutbox").checked){
+        document.getElementsByClassName("dark").disabled = false;
+    }
+    if(!document.getElementById("cutbox").checked){
+        document.getElementsByClassName("dark").disabled = true;
+    }
 }
