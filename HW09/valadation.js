be724900be70febed6c64caa56a92e1e6f0b0eff
URCOzxc <urco.dm@gmail.com>
2023-04-13 21:27:17 -0400

    4

--- HW09/valadation.js
+++ HW09/valadation.js
@@ -1,12 +1,17 @@
-var cutbutton = document.getElementById("cutbox");
+let cutbutton = null;
+
+function setvar() {
+    const cutbutton = document.getElementById("cutbox");
+}
 
 function disablebutton() {
-    if(cutbutton.checked){
+    if(cutbutton.checked) {
         document.getElementsByClassName("dark").disabled = false;
     }
-    if(!cutbutton.checked){
+    if(!cutbutton.checked) {
         document.getElementsByClassName("dark").disabled = true;
     }
 }
 
+document.addEventListener('load', setvar());
 cutbutton.addEventListener('click', disablebutton());
