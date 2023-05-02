b874c8aceb0874033dfe47d0bc697375a259e0bc
URCOzxc <urco.dm@gmail.com>
2023-05-01 20:48:40 -0400

    things

---
+++ learningJSArrays/array.js
@@ -0,0 +1,7 @@
+// The array .at() method takes an integer value and returns the item at that index, for both positive and negative integers.
+// Negative integers count backwords from the last array item.
+const condiments = ["ketchup", "mayonnaise", "mustard"];
+const atWay = condiments.at(-3);
+const bracketnotation = condiments[0];
+console.log("atWay " + atWay);
+console.log("bracketnotation " + bracketnotation);
